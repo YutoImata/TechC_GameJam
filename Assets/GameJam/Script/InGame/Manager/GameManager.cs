7fad9a2f1e4793a5d9d9aaeb50b1474507a6934e
YutoImata <yuto.imata.wine@gmail.com>
2025-08-02 18:51:16 +0900

    exeファイルでもデバックが出るように処理を追加中

---
+++ Assets/GameJam/Script/InGame/Manager/GameManager.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Tech.C
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        [SerializeField]
+        private bool isDebug;
+
+        public bool IsDebug => isDebug;
+    }
+}
