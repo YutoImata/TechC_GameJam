4784e732334c9677fdc44fd669840481a6815b22
Syuto256 <SBA25J10002@edu.sba.ac.jp>
2025-08-05 15:53:14 +0900

    gaugeのUIを変更しました

---
+++ Assets/GameJam/Script/UI/GaugeColor.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugecColor : MonoBehaviour
+{
+    // Inspectorから設定できるようにPublicでSliderコンポーネントを定義
+    public Slider gaugeSlider;
+
+    // ゲージの塗りつぶし部分のImageコンポーネントを定義
+    public Image fillImage;
+
+    // Start関数でゲージの値が変わったときに呼び出されるメソッドを登録
+    void Start()
+    {
+        // もしgaugeSliderが設定されていれば、イベントリスナーを追加
+        if (gaugeSlider != null)
+        {
+            // スライダーの値が変更されたときにOnValueChangedメソッドを呼び出すように設定
+            gaugeSlider.onValueChanged.AddListener(OnValueChanged);
+        }
+    }
+
+    // スライダーの値が変更されたときに実行されるメソッド
+    public void OnValueChanged(float value)
+    {
+        // ゲージの値が0の時
+        if (value == 0)
+        {
+            // fillImage（塗りつぶし部分）の色を完全に透明にする
+            // new Color(R, G, B, A) のA（アルファ値）を0にする
+            fillImage.color = new Color(fillImage.color.r, fillImage.color.g, fillImage.color.b, 0f);
+        }
+        else
+        {
+            // ゲージの値が0以外の時
+            // fillImageの色を元の不透明な色に戻す
+            // new Color(R, G, B, A) のA（アルファ値）を1にする
+            fillImage.color = new Color(fillImage.color.r, fillImage.color.g, fillImage.color.b, 1f);
+        }
+    }
+}
